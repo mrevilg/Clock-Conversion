9a5b493916524bd5f006d22d1fe43eaf99a9656e
Stephen Clements <mrevilg@gmail.com>
2019-06-18 17:29:56 -0500

    Inital File Uploads

---
+++ ClockDict.py
@@ -0,0 +1,3 @@
+
+
+user_time = input("Please input any 24hr time in the following format: hh:mm - ")
--- ClockImport.py
+++ ClockImport.py
@@ -1,7 +1,12 @@
+
+# from (module) datetime import (class) datetime
 from datetime import datetime
 
+# prompt user for any time in 24hr hh:mm formatting
 user_time = input("Please input any 24hr time in the following format: hh:mm - ")
 
+# Parse string into datetime:class and then Format into converted version
 user_time = datetime.strptime(user_time, '%H:%M').strftime('%I:%M %p')
 
+# print converted version
 print (user_time)
