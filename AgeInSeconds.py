b3ff4f65e1aed8b075ddf3a7b07f740b82590ec8
Stephen Clements <mrevilg@gmail.com>
2019-06-24 18:13:11 -0500

    Age In Seconds

---
+++ AgeInSeconds.py
@@ -0,0 +1,13 @@
+from datetime import datetime, time
+
+birth_date = input ("Please entre your birthdate yyyy-mm-dd hh:mm:ss")
+
+def date_diff_in_Seconds(dt2, dt1):
+  timedelta = dt2 - dt1
+  return timedelta.days * 24 * 3600 + timedelta.seconds
+#Specified date
+date1 = datetime.strptime(f'{birth_date}', '%Y-%m-%d %H:%M:%S')
+#Current date
+date2 = datetime.now()
+print("\n%d seconds" %(date_diff_in_Seconds(date2, date1)))
+print()
